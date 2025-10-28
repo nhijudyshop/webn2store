bb085dd3218c3b24eae80475e0d60874b58ec4cb
[dyad] <nhijudyshop@gmail.com>
2025-10-28 09:41:52 +0700

    [dyad] Thiết lập Service Role Key và cài đặt dotenv - wrote 4 file(s), added dotenv package(s)

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+require('dotenv').config(); // Load environment variables from .env file
 
 // Import routes
 const settingsRoutes = require("./routes/settings-routes");
